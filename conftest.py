b3bdabc7e7d1d4543b006f3c4d582b4c577d18b8
Daniel <roghoh@gmail.com>
2024-05-23 17:53:50 +0400

    working tests: all

--- conftest.py
+++ conftest.py
@@ -13,5 +13,5 @@ def driver(request):
     options.add_argument("--window-size=1920,1080")
     driver = webdriver.Chrome(options=options)
     request.cls.driver = driver
-    yield
+    yield driver
     driver.quit()
